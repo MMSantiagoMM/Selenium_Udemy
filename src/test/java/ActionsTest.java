efac31b70334e561ee319e72d35c4113d4156a0f
MMSantiagoMM <santiagomosquera2@gmail.com>
2024-04-11 16:21:06 -0500

    Learning automation with Selenium from Udemy

--- src/test/java/ActionsTest.java
+++ src/test/java/ActionsTest.java
@@ -3,16 +3,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class ActionsTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testOne() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
         Actions actions = new Actions(driver);
         WebElement element = driver.findElement(By.id("nav-link-accountList"));
 
         actions.moveToElement(element).build().perform();
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("Hello");
     }
 
 
